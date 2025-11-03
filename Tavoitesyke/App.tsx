419eb9af098967ba7cbafe07c0082be3dcfb82f7
msotkasi20 <msotkasi20@gmail.com>
2025-11-03 16:15:32 +0200

    second commit

--- Tavoitesyke/App.tsx
+++ Tavoitesyke/App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react'
 
 export default function App() {
+
+  const [age, setAge] = useState<string>("")
+  const ageAsNumber: number = Number(age)
+
+  const lowerLimit: number = !isNaN(ageAsNumber) ? (220-ageAsNumber) * 0.65 : 0
+  const upperLimit: number = !isNaN(ageAsNumber) ? (220-ageAsNumber) * 0.85 : 0
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>Heart Rate Limits Calculator</Text>
+      <Text>Enter your age:</Text>
+      <TextInput
+        keyboardType='number-pad'
+        value={age}
+        onChangeText={setAge}
+      />
+      <Text>{lowerLimit.toFixed(2)}</Text>
+      <Text>{upperLimit.toFixed(2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +30,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+    marginLeft: 20
   },
+  heading: {
+    fontSize: 24
+  }
+
 });
