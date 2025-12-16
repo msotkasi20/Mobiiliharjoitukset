e0466f310264ad4148ae65fbb586537a9a9a162c
msotkasi20 <msotkasi20@gmail.com>
2025-12-16 17:17:42 +0200

    uusi commit

--- Barcodereader/App.tsx
+++ Barcodereader/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useCameraPermissions, BarcodeScanningResult } from 'expo-camera'
